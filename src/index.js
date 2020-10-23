36124eec4de1542fc9aab2359a1d2b5c94c17c54
DINESH KUMAR <30974908+studyskymate@users.noreply.github.com>
2020-10-23 17:46:44 +1200

    mdb bootstrap added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import FooterPage from './components/FooterPage';
+import Header from './components/layout/Header';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
  // <React.StrictMode>
     <BrowserRouter>
     <App />
+
     </BrowserRouter>,
   //</React.StrictMode>,
   document.getElementById('root')
